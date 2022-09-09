8c4cbacf30ae734251f940bda5ed3b14065e5a0e
AMIT KUSHWAHA <amit.kushwaha@somaiya.edu>
2022-09-10 02:00:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function func() {
   if (yourguess.value == ans) {
     message.textContent = "that's correct";
     document.querySelector('.number').textContent = yourguess.value;
+    document.querySelector('body').style.backgroundColor = '#59CE8F';
     if (highScore.textContent < score.textContent)
       highScore.textContent = score.textContent;
   } else if (yourguess.value < ans) {
@@ -27,15 +28,32 @@ function func() {
     message.textContent = "that's too high";
     score.textContent--;
   }
+
+  changebackground();
 }
 
 function reset() {
   message.textContent = 'Start guessing..';
   yourguess.value = 0;
   score.textContent = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
   ans = Math.floor(Math.random() * 20) + 1;
 }
 
-// while (true) {
-//   func();
-// }
+function changebackground() {
+  let color_changer = 110 - (20 - score.textContent) * 10;
+
+  let color_text = `rgb(255, ${color_changer}, 26)`;
+  console.log(color_text);
+  if (color_changer < 0) color_changer = 0;
+  if (score.textContent == 0) {
+    document.querySelector('.number').textContent = 'game over';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  if (score.textContent < 0) {
+    reset();
+  }
+  document.querySelector('body').style.backgroundColor = color_text;
+}
